eb0860c89a4754cfc8f7243119634e92ea4a4e07
Sridhar <sridharb.in@gmail.com>
2021-10-05 21:45:16 +0530

    added new

---
+++ src/main/java/com/demo/app/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.demo.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+@GetMapping("/greet/{name}")
+public String greetUser(@PathVariable("name") String name) {
+	return String.format("Welcome To Chennai, %s ",name);
+}
+
+
+
+
+}
